38972175e849328ee7b354795a40fa67fd1808ce
Noble Mutoko <noblemutoko@gmail.com>
2023-09-14 12:46:55 +0100

    comments added

--- calculate-demo.py
+++ calculate-demo.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-print(1+1)
-
-if __name__ == "__main__":
+print(1+1) # print addition 1+1
+print(__name__) # print the name of the main function called on execution of the script
+if __name__ == "__main__": # testing if the script is a standalone or part of another module
     print("Do Something")
