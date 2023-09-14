1f882abb3d847e75e8a3e79384c4e104563ba9a2
Noble Mutoko <noblemutoko@gmail.com>
2023-09-14 13:05:55 +0100

    learn the sytax of python

---
+++ syntax-dem.py
@@ -0,0 +1,12 @@
+def test(): # define a function test
+	print("test")
+
+	sring = "string"
+	number = 10
+
+test()
+
+x = 1 + 2 \
+	+ 3			# indentation using backslash
+
+print(x) # Print the value of x
