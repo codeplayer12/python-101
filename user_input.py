b9de1fb4b44724714dd565ec7561ddaea1f0b17e
Noble Mutoko <noblemutoko@gmail.com>
2023-09-15 10:58:44 +0100

    learning user input

---
+++ user_input.py
@@ -0,0 +1,15 @@
+test = input()
+print(test)
+
+
+test2 = input("Enter an IP address:> ")
+print(test2)
+
+
+while True:
+	test3  = input("Enter the IP:> ")
+	print(">>> {}".format(test3))
+	if test3 == "exit":
+		break
+	else:
+		print("Exploiting...")
