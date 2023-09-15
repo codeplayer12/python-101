f21aab9b4a46ecf87a6ae11d39cbf8704a2b01cc
Noble Mutoko <noblemutoko@gmail.com>
2023-09-15 10:11:45 +0100

    conditional statements

---
+++ conditions.py
@@ -0,0 +1,28 @@
+if True:
+	print("True")
+
+if False:
+	print("False")
+
+if not False:
+	print("not False")
+
+if 1 < 1:
+	print("1 <	 1")
+elif 1<=0:
+	print("1<=0")
+else:
+	print("else 1")
+
+# Comparing <> chained with and / or
+if 1 > 0 and 0 <1:
+	print("1 > 0 and  0 < 1")
+
+if 0 > 1 or 0 <1:
+	print("0 > 1 and  0< 1")
+
+
+if 0<1: print("0<1")
+
+print("1>=1") if 1 >=1 else print("1<1") # Tenary format if else
+
